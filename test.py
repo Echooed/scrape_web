cb7aa4f1772ed31c187d084382e74fa936ca3218
Michael <echoechodeveloper@gmail.com>
2024-11-02 06:26:39 +0100

    ...

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ import re
 import pandas as pd
 import time
 
-
+# fetch website and pass into variable
 response = requests.get('https://www.timesjobs.com/candidate/job-search.html?from=submit&luceneResultSize=25&txtKeywords=data&postWeek=60&searchType=personalizedSearch&actualTxtKeywords=data&searchBy=0&rdoOperator=OR&pDate=I&sequence=1&startPage=1')
+
 def find_jobs():
     if response.status_code == 200:
         html_text =response.text
@@ -18,10 +19,8 @@ def find_jobs():
             job_title = job_box.find('a').text.strip()
             more_details = job_box.a['href']
             
-                # Locate the skills container and pass it to the function
+            # Locate the skills container and pass it to the function
             skills_container = job_box.find_all('div', class_='more-skills-sections')
-        
-            
 
             # function to extract skills from with the span tags using regex
             def extract_skills(skills_container):
@@ -46,4 +45,4 @@ if __name__ == '__main__':
           find_jobs()
           time_wait = 10
           print(f'waiting {time_wait} seconds...')
-          time.sleep(time_wait * 60)
+          time.sleep(time_wait)
